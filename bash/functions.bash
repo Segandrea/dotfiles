0034577f3f296452c41cdaca15b369b12bfdc406
Segandrea <andreasegantini97@gmail.com>
2022-07-12 12:15:55 +0200

    add .xz and .txz to extract function

--- bash/functions.bash
+++ bash/functions.bash
@@ -94,6 +94,8 @@ extract(){
     case "$1"  in
       *.tar.bz2)   tar xjf "$1" ;;
       *.tar.gz)    tar xzf "$1" ;;
+      *.tar.xz)    tar xJf "$1" ;;
+      *.txz)       tar xJf "$1" ;;
       *.bz2)       bunzip2 "$1" ;;
       *.rar)       unrar x "$1" ;;
       *.gz)        gunzip "$1" ;;
