207893f7e053051afa98434fd25efd8988e91c55
Segandrea <andreasegantini97@gmail.com>
2022-08-13 15:17:40 +0200

    alias: fix grep headphones before sed

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -87,7 +87,7 @@ spicetify restore backup apply'
 alias upgradeall='yay -Syu && flatpak update && rustup update'
 
 # connect and disconnect sony wf-c500
-alias btcs='bluetoothctl power on && bluetoothctl connect $(bluetoothctl paired-devices | sed "s/^Device \(.*\) WF-C500$/\1/")'
+alias btcs='bluetoothctl power on && bluetoothctl connect $(bluetoothctl paired-devices | grep -i "WF-C500" | sed "s/^Device \(.*\) WF-C500$/\1/")'
 alias btds='bluetoothctl disconnect $(bluetoothctl paired-devices | sed "s/^Device \(.*\) WF-C500$/\1/")'
 
 # dump and load  settings
