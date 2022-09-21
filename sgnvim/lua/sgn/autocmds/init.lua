bd900e684d043e7cc4839ae718085e9b475c9c92
Segandrea <andreasegantini97@gmail.com>
2022-09-21 16:56:13 +0200

    removed sgnvim

--- sgnvim/lua/sgn/autocmds/init.lua
+++
@@ -1,9 +0,0 @@
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd('VimEnter', { pattern = '*', command = 'set formatoptions-=cro' })
-autocmd('InsertEnter', { pattern = '*', command = 'norm zz' })
-autocmd('InsertEnter', { pattern = '*', command = 'set nohlsearch' })
-autocmd('BufReadPost', { pattern = '*', command = [[if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit' | execute "normal! g`\"" | endif]] })
-
--- TODO: add autocmds specific for filetypes
