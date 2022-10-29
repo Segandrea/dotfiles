876fdbc80f2b2d32ee21be2119b241841ce011bb
Segandrea <andreasegantini97@gmail.com>
2022-10-29 21:02:02 +0200

    fix bash bindings are needed for both insert and command mode

--- bash/keybindings.bash
+++ bash/keybindings.bash
@@ -7,4 +7,5 @@
 # The keybindings defined here only work inside bash shell
 
 # Bind Ctrl+g to `goto()`
-bind '"\C-g":"goto && clear\n"'
+bind -m vi-insert '"\C-g":"goto && clear \n"'
+bind -m vi-command '"\C-g":"i goto && clear \n"' # 'i' is to enter insert
