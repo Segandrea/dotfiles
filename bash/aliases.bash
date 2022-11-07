6d87a00dea36351a4e3efab9bdfc2dc1e13104d1
Segandrea <andreasegantini97@gmail.com>
2022-11-07 17:58:08 +0100

    small improvement in QOL for markdown editing

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -85,4 +85,4 @@ alias dconfdump='dconf dump / > dumped_settings.ini'
 alias dconfload='dconf load / < dumped_settings.ini'
 
 # open ignoramus
-alias nora='[[ -d "$IGNORAMUS_DIR" ]] && cd "$IGNORAMUS_DIR" && "$EDITOR" "$IGNORAMUS_DIR/index.md"'
+alias nora='[[ -d "$IGNORAMUS_DIR" ]] && cd "$IGNORAMUS_DIR" && "$EDITOR" "$IGNORAMUS_DIR/index.md" && cd "$OLDPWD"'
