93e0c9d6ada650d6de423222d51e11a016e4d0e5
Segandrea <andreasegantini97@gmail.com>
2022-11-09 19:24:21 +0100

    bugfix

--- bash/.config/bash/keybindings.bash
+++ bash/.config/bash/keybindings.bash
@@ -5,11 +5,16 @@
 # (Eg. bash functions can't be called inside the python interpreter)
 #
 # The keybindings defined here only work inside bash shell
+#
+# INFO: The string containing the keybinding should always be prefixed with
+#       `\C-c\n` which stands for `<Ctrl-c><Return>`.
+#       Doing this avoid the concatenation of the command with eventual
+#       dangling strings.
 
 # Bind Ctrl+g to `goto()`
-bind -m vi-insert '"\C-g":"goto && clear\n"'
-bind -m vi-command '"\C-g":"igoto && clear\n"' # 'i' is to enter insert
+bind -m vi-insert '"\C-g":"\C-c\ngoto && clear\n"'
+bind -m vi-command '"\C-g":"\C-c\ngoto && clear\n"' # 'i' is to enter insert
 
 # Bind Ctrl+h to `gethelp()`
-bind -m vi-insert '"\C-h":"gethelp && clear\n"'
-bind -m vi-command '"\C-h":"igethelp && clear\n"'
+bind -m vi-insert '"\C-h":"\C-c\ngethelp && clear\n"'
+bind -m vi-command '"\C-h":"\C-c\ngethelp && clear\n"'
