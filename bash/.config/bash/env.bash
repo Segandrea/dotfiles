7becec1ba0bc7734c76c6c8981051c86ddd0989d
Andrea Segantini <andreasegantini97@gmail.com>
2023-03-20 23:53:40 +0100

    bash env cleanup

--- bash/.config/bash/env.bash
+++ bash/.config/bash/env.bash
@@ -21,10 +21,19 @@ for index in "${!script_and_bin_dirs[@]}"; do
   fi
 done
 
+# User XDG Base Directory Specifications
+export XDG_CACHE_HOME="$HOME/.cache"       # analogous to /var/cache/
+export XDG_CONFIG_HOME="$HOME/.config"     # analogous to /etc/
+export XDG_DATA_HOME="$HOME/.local/share"  # analogous to /usr/share/
+export XDG_STATE_HOME="$HOME/.local/state" # analogous to /var/lib/
+
+
 export PATH="${PATH}${my_paths}"                   # add custom paths to PATH
 export HISTCONTROL="ignoreboth"                    # no dups and no blank starting lines
-export HISTFILE="$XDG_STATE_HOME/bash/history"     # use .bash_history as file for storing history
+[[ ! -d "$XDG_STATE_HOME/bash" ]] && mkdir -p -- "$XDG_STATE_HOME/bash"
+export HISTFILE="$XDG_STATE_HOME/bash/history"     # place bash history inside XDG_STATE directory
 export PAGER="less"                                # use less as pager
+[[ ! -d "$XDG_STATE_HOME/less" ]] && mkdir -p -- "$XDG_STATE_HOME/less"
 export LESSHISTFILE="$XDG_STATE_HOME/less/history" # place less history inside XDG_STATE directory
 export EDITOR="nvim"                               # use vim as default editor
 export BROWSER="google-chrome"                     # use chrome as default browser
@@ -32,12 +41,6 @@ export NVM_DIR="$HOME/.nvm"                        # nvm
 export FUNCNEST=100                                # limits recursive functions, see 'man bash'
 export FZF_DEFAULT_OPTS='--color=bg+:#282a36'      # fzf highlight choice with background color (unseeable) adding cargo executables to path (it is some how already set)
 export INPUTRC="$HOME/.config/readline/.inputrc"   # readline configuration
-
-# User XDG Base Directory Specifications
-export XDG_CACHE_HOME="$HOME/.cache"       # analogous to /var/cache/
-export XDG_CONFIG_HOME="$HOME/.config"     # analogous to /etc/
-export XDG_DATA_HOME="$HOME/.local/share"  # analogous to /usr/share/
-export XDG_STATE_HOME="$HOME/.local/state" # analogous to /var/lib/
 
 # my most used directories
 export DOCUMENTS_DIR="$HOME/Documents"
