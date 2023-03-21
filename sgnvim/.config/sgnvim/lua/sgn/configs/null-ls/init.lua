d06e7a71105ef321a06e283acfa24dda4a9e9e30
Andrea Segantini <andreasegantini97@gmail.com>
2023-03-21 19:28:39 +0100

    wip: adding eslint_d configuration to dotfiles

--- sgnvim/.config/sgnvim/lua/sgn/configs/null-ls/init.lua
+++ sgnvim/.config/sgnvim/lua/sgn/configs/null-ls/init.lua
@@ -11,7 +11,7 @@ nls.setup({
     -- [[ Formatting ]]
     formatting.prettier, -- webdev + md, yml
     -- [[ Diagnostic ]]
-    diagnostics.eslint, -- webdev
+    diagnostics.eslint_d, -- webdev
     diagnostics.shellcheck, -- sh
     diagnostics.markdownlint.with({
       extra_args = { '-c', vim.fn.expand("$XDG_CONFIG_HOME/markdownlint/.markdownlint.yaml") },
