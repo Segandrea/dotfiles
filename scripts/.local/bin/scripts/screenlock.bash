f838a8e53ee1b4c18e4cefb5783cb9260726e1b1
segafrega <andreasegantini97@gmail.com>
2023-12-08 20:16:01 +0100

    screenlock script now also pauses and resumes gnome-pomodoro

--- scripts/.local/bin/scripts/screenlock.bash
+++ scripts/.local/bin/scripts/screenlock.bash
@@ -1,28 +1,34 @@
 #!/usr/bin/env bash
 
-# Poweroff bluetoothctl when the screen is locked
-# then power it on again when the screen is unlocked
-# trying to reconnect automatically to all the bluetooth
-# devices that were connected before.
+# Poweroff bluetoothctl when the screen is locked, also pause
+# gnome-pomodoro and spotify then resume gnome-pomodoro and restart
+# bluetooth on screen unlock reconnecting all the bluetooth devices
+# that were connected before.
 #
-# NOTE: there is no need to launch it, it should be launched at startup
+# NOTE: there is no need to launch it, this is a script launched at startup
 
 declare is_unlocked=true
 declare connected_devices
+# monitor gnome screensaver to know when screen is locked
 dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
   while read -r is_locking; do
     case "${is_locking}" in
       *"boolean true"*)
+        # this "if" is needed because the signal is doubled
         if $is_unlocked; then
           is_unlocked=false
+          gnome-pomodoro --pause
+          # Stop spotify
           dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
           connected_devices="$(bluetoothctl devices Connected | awk '{print $2}')"
           bluetoothctl power off
         fi
       ;;
       *"boolean false"*)
+        # this "if" is needed because the signal is doubled
         if ! $is_unlocked; then
           is_unlocked=true
+          gnome-pomodoro --resume
           bluetoothctl power on
           sleep 5
           for device in $connected_devices; do
