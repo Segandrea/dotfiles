28b5f36f16b599c0b53e612b252d596eab4d1d76
segafrega <andreasegantini97@gmail.com>
2024-03-07 10:48:47 +0100

    edit zoxide fzf variable

--- bash/.config/bash/env.bash
+++ bash/.config/bash/env.bash
@@ -41,7 +41,7 @@ export NVM_DIR="$XDG_CONFIG_HOME/nvm"              # nvm
 export FUNCNEST=100                                # limits recursive functions, see 'man bash'
 export FZF_DEFAULT_OPTS='--color=bg+:#282a36'      # fzf highlight choice with background color (unseeable) adding cargo executables to path (it is some how already set)
 # default fzf options for zoxide use
-export _ZO_FZF_OPTS="--exact --no-info --reverse --preview='tree -CL 2 {}' --delimiter='/' --pointer='➜' --prompt='Go to: '"
+export _ZO_FZF_OPTS="--color=bg+:#282a36 --exact --no-info --reverse --no-sort --pointer='➜' --prompt='Go to: '"
 
 # my most used directories
 export DOCUMENTS_DIR="$HOME/Documents"
