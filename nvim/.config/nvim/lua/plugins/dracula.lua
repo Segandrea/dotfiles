a59813ce36a84030e530069a93931e8ec8027705
segafrega <andreasegantini97@gmail.com>
2024-04-19 14:58:49 +0200

    changed highlight color for search under cursor

--- nvim/.config/nvim/lua/plugins/dracula.lua
+++ nvim/.config/nvim/lua/plugins/dracula.lua
@@ -1,5 +1,8 @@
 return {
   'Mofiqul/dracula.nvim',
   commit = '8d8bddb',
-  config = function() vim.cmd[[colorscheme dracula]] end
+  config = function()
+    vim.cmd.colorscheme('dracula')
+    vim.cmd.highlight('CurSearch guifg=#44475A guibg=#6272a4')
+  end
 }
