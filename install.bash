74b1741070b73666ed249f2416ae486fe41ca622
segafrega <andreasegantini97@gmail.com>
2024-12-04 18:32:02 +0100

    renamed installation script and added dnf and flatpak packages to it

--- install.bash
+++ install.bash
@@ -24,6 +24,17 @@ dir2link=(
   "vim/"
 )
 
+# Enabling some copr
+sudo dnf copr enable atim/starship
+# Installing packages with dnf
+dnf_packages='alacritty calibre dconf fd-find fzf git gnome-shell-extension-pop-shell kitty ncompress neovim p7zip-plugins rustup starship stow tealdeer tmux unrar vim zoxide'
+sudo dnf install "${dnf_packages}"
+# Installing packages with flatpak NB. if it doesn't work make an array from the variable and cycle through it
+flatpak_packages='androidstudio brave embellish extensionmanager extensions flatseal intellij spotify telegram'
+sudo flatpak install "${flatpak_packages}"
+# fonts
+echo "${info} Nerd fonts can be installed with embellish"
+
 if [[ -x "$(command -v stow)" ]]; then
   echo "${info} Linking files with stow"
   stow --target="$HOME" -S "${dir2link[@]}" && echo "${succ} configuration stowed" || echo "${error} failed to stow configuration"
