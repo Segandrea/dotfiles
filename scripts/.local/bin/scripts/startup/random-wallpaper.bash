51bbb0df9aa51bbe80d325b3826ed49932e5c88f
segafrega <andreasegantini97@gmail.com>
2025-04-13 12:47:04 +0200

    fix: random wallpaper at startup

--- scripts/.local/bin/scripts/startup/random-wallpaper.bash
+++ scripts/.local/bin/scripts/startup/random-wallpaper.bash
@@ -2,14 +2,15 @@
 
 shopt -s nullglob
 
-declare -a images=( "$PICTURES_DIR/Gnome/Wallpaper"/*.{jpg,png,jpeg,JPG,PNG,JPEG} )
+declare wallpaper_dir_path="/home/$(whoami)/Pictures/Gnome/Wallpaper"
+declare -a images=( "${wallpaper_dir_path}"/*.{jpg,png,jpeg,JPG,PNG,JPEG} )
 declare wallpaper_of_the_day="${images[$(shuf --input-range=0-$((${#images[@]}-1)) --head-count=1)]}"
 
 if [[ -n "${wallpaper_of_the_day}" ]]; then
   gsettings set org.gnome.desktop.background picture-uri "file://${wallpaper_of_the_day}" && \
     gsettings set org.gnome.desktop.background picture-uri-dark "file://${wallpaper_of_the_day}" && \
     gsettings set org.gnome.desktop.screensaver picture-uri "file://${wallpaper_of_the_day}" && \
-    echo "the new wallpaper is ${wallpaper_of_the_day}"
+    notify-send --app-name="Random wallpaper" --expire-time=2000 --icon="${wallpaper_of_the_day}" "the new wallpaper is '${wallpaper_of_the_day##*/}'"
 else
-  echo "Choose one of the wallpaper or download one and retry"
+  notify-send "Choose one of the wallpaper or download one and retry"
 fi
