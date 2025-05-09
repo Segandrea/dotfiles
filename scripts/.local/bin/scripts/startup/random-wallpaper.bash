e29dd9a17c69800ffdcbf11740f12e126f825cc8
segafrega <andreasegantini97@gmail.com>
2025-05-09 23:08:22 +0200

    fix: removed notification telling which image is on as wallpaper

--- scripts/.local/bin/scripts/startup/random-wallpaper.bash
+++ scripts/.local/bin/scripts/startup/random-wallpaper.bash
@@ -9,8 +9,7 @@ declare wallpaper_of_the_day="${images[$(shuf --input-range=0-$((${#images[@]}-1
 if [[ -n "${wallpaper_of_the_day}" ]]; then
   gsettings set org.gnome.desktop.background picture-uri "file://${wallpaper_of_the_day}" && \
     gsettings set org.gnome.desktop.background picture-uri-dark "file://${wallpaper_of_the_day}" && \
-    gsettings set org.gnome.desktop.screensaver picture-uri "file://${wallpaper_of_the_day}" && \
-    notify-send --app-name="Random wallpaper" --expire-time=2000 --icon="${wallpaper_of_the_day}" "the new wallpaper is '${wallpaper_of_the_day##*/}'"
+    gsettings set org.gnome.desktop.screensaver picture-uri "file://${wallpaper_of_the_day}"
 else
   notify-send "Choose one of the wallpaper or download one and retry"
 fi
